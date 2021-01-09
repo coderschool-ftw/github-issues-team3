fa7eeb7fb76621a19aabd99b58b0657518127c43
phuong97 <trantran.tnl30@gmail.com>
2021-01-09 10:03:02 +0700

    adding fetch data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
-
+import Nav from "./components/Nav";
+import SearchForm from "./components/SearchForm";
 function App() {
+  const [issues, setIssues] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      if (url !== "") {
+        let data = await fetch(url);
+        let json = await data.json();
+        console.log(json);
+        setIssues(json);
+      }
+    };
+    getData();
+  }, [url]);
+  const handleClick = () => {
+    setUrl(`https://api.github.com/repos/${searchTerm}/issues`);
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <Container>
-      <h1 className="text-center">GitHub Issues Browser</h1>
-      <p>Fill in files from /src/components.</p>
+      <Nav />
+      <SearchForm
+        handleChange={handleChange}
+        handleClick={handleClick}
+        value={searchTerm}
+      />
     </Container>
   );
 }
