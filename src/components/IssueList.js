0cbf3ff166691d8ff4cc52578f40dea68f61d1c0
Prince Slater <everythinginmoderation2@gmail.com>
2021-01-10 00:46:36 +0700

    .

--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { ListGroup, Row, Col, Figure, Image } from "react-bootstrap";
 
-const IssueList = ({issues, handleIssueClick}) => {
+const IssueList = ({ issues, handleIssueClick }) => {
   return (
-      <>
+    <>
       <h2>Issue List</h2>
-      <ul>
-          {issues.map(i => <li key={i.id}><a href='#' onClick={() => handleIssueClick(i)}>{i.title}</a></li>)}
-      </ul>
-      </>
-  )
+      <ListGroup>
+        {issues.map((i) => (
+          <ListGroup.Item key={i.id} action>
+            <Row>
+            <Col><a href={i.url} onClick={() => handleIssueClick(i)}>{i.title}</a>   Issue #{i.number}</Col>
+            <Col>{i.labels}</Col><Col>{i.user.login}<Figure.Image width={70}
+     height={70} src={i.user.avatar_url} alt={i.user.avatar_url} thumbnail={true}/></Col><Col>{i.updated_at}</Col>
+     </Row>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </>
+  );
 };
 
-
-export default IssueList
+export default IssueList;
